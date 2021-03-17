9d5aef2c57bf7a5df9b8f651bfbb5f5444f59d53
algernon-A <aeneas@aloku.net>
2021-03-18 10:31:27 +1100

    Fix MessageBox content width on resize.

--- ZoningAdjuster/Code/Notifications/MessageBoxBase.cs
+++ ZoningAdjuster/Code/Notifications/MessageBoxBase.cs
@@ -6,7 +6,8 @@ using System;
 using System.Linq;
 using UnityEngine;
 
-namespace PloppableRICO.MessageBox
+
+namespace ZoningAdjuster.MessageBox
 {
     /// <summary>
     /// Base class for displaying modal message boxes.
@@ -211,8 +212,9 @@ namespace PloppableRICO.MessageBox
             // Set height.
             height = titleBar.height + mainPanel.height + buttonPanel.height + (Padding * 2f);
 
-            // Position main panel under title bar.
+            // Position main panel under title bar and set width.
             mainPanel.relativePosition = new Vector2(0, titleBar.height);
+            mainPanel.width = width - (mainPanel?.verticalScrollbar?.width ?? 0) - 3f;
 
             // Position button panel under main panel.
             buttonPanel.relativePosition = new Vector2(0, titleBar.height + mainPanel.height + Padding);
