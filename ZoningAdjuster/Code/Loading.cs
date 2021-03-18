b2e1de2bdba45bf941c9acca6d6029566ec4ff3b
algernon-A <aeneas@aloku.net>
2021-03-18 11:41:09 +1100

    Add mod conflict detection and notification.

--- ZoningAdjuster/Code/Loading.cs
+++ ZoningAdjuster/Code/Loading.cs
@@ -9,17 +9,64 @@ namespace ZoningAdjuster
 	/// Main loading class: the mod runs from here.
 	/// </summary>
 	public class Loading : LoadingExtensionBase
-	{
-		/// <summary>
-		/// Called by the game when level loading is complete.
-		/// </summary>
-		/// <param name="mode">Loading mode (e.g. game, editor, scenario, etc.)</param>
-		public override void OnLevelLoaded(LoadMode mode)
+    {
+        // Status flags.
+        private static bool harmonyLoaded = false;
+        private static bool conflictingMod = false;
+
+
+        /// <summary>
+        /// Called by the game when the mod is initialised at the start of the loading process.
+        /// </summary>
+        /// <param name="loading">Loading mode (e.g. game, editor, scenario, etc.)</param>
+        public override void OnCreated(ILoading loading)
+        {
+            base.OnCreated(loading);
+
+            // Don't do anything if not in game (e.g. if we're going into an editor).
+            if (loading.currentMode != AppMode.Game)
+            {
+                Logging.KeyMessage("not loading into game, skipping activation");
+
+                // Set harmonyLoaded and PatchOperating flags to suppress Harmony warning when e.g. loading into editor.
+                harmonyLoaded = true;
+
+                // Unload Harmony patches and exit before doing anything further.
+                Patcher.UnpatchAll();
+                return;
+            }
+
+            // Ensure that Harmony patches have been applied.
+            harmonyLoaded = Patcher.Patched;
+            if (!harmonyLoaded)
+            {
+                Logging.Error("Harmony patches not applied; aborting");
+                return;
+            }
+
+            // Check for mod conflicts.
+            if (ModUtils.IsModConflict())
+            {
+                // Conflict detected.
+                conflictingMod = true;
+
+                // Unload Harmony patches and exit before doing anything further.
+                Patcher.UnpatchAll();
+                return;
+            }
+        }
+
+
+        /// <summary>
+        /// Called by the game when level loading is complete.
+        /// </summary>
+        /// <param name="mode">Loading mode (e.g. game, editor, scenario, etc.)</param>
+        public override void OnLevelLoaded(LoadMode mode)
 		{
 			base.OnLevelLoaded(mode);
 
 			// Check to see that Harmony 2 was properly loaded.
-			if (!Patcher.Patched)
+			if (!harmonyLoaded)
 			{
 				// Harmony 2 wasn't loaded.
 				Logging.Error("Harmony patches not applied; aborting");
@@ -28,7 +75,7 @@ namespace ZoningAdjuster
 				ListMessageBox harmonyBox = MessageBoxBase.ShowModal<ListMessageBox>();
 
 				// Key text items.
-				harmonyBox.AddParas(Translations.Translate("ERR_HAR0"), Translations.Translate("ZAM_ERR_HAR"), Translations.Translate("ZAM_ERR_FAT"), Translations.Translate("ERR_HAR1"));
+				harmonyBox.AddParas(Translations.Translate("ERR_HAR0"), Translations.Translate("ZMD_ERR_HAR"), Translations.Translate("ZMD_ERR_FAT"), Translations.Translate("ERR_HAR1"));
 
 				// List of dot points.
 				harmonyBox.AddList(Translations.Translate("ERR_HAR2"), Translations.Translate("ERR_HAR3"));
@@ -40,8 +87,27 @@ namespace ZoningAdjuster
 				return;
 			}
 
-			// Initialise zoning tool.
-			ToolsModifierControl.toolController.gameObject.AddComponent<ZoningTool>();
+			// Check to see if a conflicting mod has been detected.
+			if (conflictingMod)
+			{
+				// Mod conflict detected - display warning notification and exit.
+				ListMessageBox modConflictBox = MessageBoxBase.ShowModal<ListMessageBox>();
+
+				// Key text items.
+				modConflictBox.AddParas(Translations.Translate("ERR_CON0"), Translations.Translate("ZMD_ERR_CON0"), Translations.Translate("ZMD_ERR_FAT"), Translations.Translate("ERR_CON1"));
+
+				// Add conflicting mod name(s).
+				modConflictBox.AddList(ModUtils.conflictingModNames.ToArray());
+
+				// Closing para.
+				modConflictBox.AddParas(Translations.Translate("ZMD_ERR_CON1"));
+
+                // Don't do anything further.
+                return;
+            }
+
+            // Initialise zoning tool.
+            ToolsModifierControl.toolController.gameObject.AddComponent<ZoningTool>();
 
 			// Add panel button.
 			ZoningAdjusterButton.CreateButton();
--- ZoningAdjuster/Code/Utils/ModUtils.cs
+++ ZoningAdjuster/Code/Utils/ModUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Reflection;
 using ICities;
 using ColossalFramework.Plugins;
 
@@ -12,6 +13,10 @@ namespace ZoningAdjuster
     /// </summary>
     internal static class ModUtils
     {
+        // List of conflcting mod names.
+        internal static List<string> conflictingModNames;
+
+
         /// <summary>
         /// Returns the filepath of the current mod assembly.
         /// </summary>
@@ -45,6 +50,60 @@ namespace ZoningAdjuster
             // If we got here, then we didn't find the assembly.
             Logging.Message("assembly path not found");
             throw new FileNotFoundException(ZoningAdjusterMod.ModName + ": assembly path not found!");
+        }
+
+
+        /// <summary>
+        /// Checks for any known fatal mod conflicts.
+        /// </summary>
+        /// <returns>True if a mod conflict was detected, false otherwise</returns>
+        internal static bool IsModConflict()
+        {
+            // Initialise flag and list of conflicting mods.
+            bool conflictDetected = false;
+            conflictingModNames = new List<string>();
+
+            // Iterate through the full list of plugins.
+            foreach (PluginManager.PluginInfo plugin in PluginManager.instance.GetPluginsInfo())
+            {
+                foreach (Assembly assembly in plugin.GetAssemblies())
+                {
+                    switch (assembly.GetName().Name)
+                    {
+                        case "NetworkExtensions2":
+                            // Network Extensions 2.
+                            conflictDetected = true;
+                            conflictingModNames.Add("Network Extensions 2");
+                            break;
+                        case "VanillaGarbageBinBlocker":
+                            // Garbage Bin Controller
+                            conflictDetected = true;
+                            conflictingModNames.Add("Garbage Bin Controller");
+                            break;
+                        case "Painter":
+                            // Painter - this one is trickier because both Painter and Repaint use Painter.dll (thanks to CO savegame serialization...)
+                            if (plugin.userModInstance.GetType().ToString().Equals("Painter.UserMod"))
+                            {
+                                conflictDetected = true;
+                                conflictingModNames.Add("Painter");
+                            }
+                            break;
+                    }
+                }
+            }
+
+            // Was a conflict detected?
+            if (conflictDetected)
+            {
+                // Yes - log each conflict.
+                foreach (string conflictingMod in conflictingModNames)
+                {
+                    Logging.Error("Conflicting mod found: ", conflictingMod);
+                }
+                Logging.Error("exiting due to mod conflict");
+            }
+
+            return conflictDetected;
         }
     }
 }
