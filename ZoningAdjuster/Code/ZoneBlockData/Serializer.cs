7b6ca4e15be5bac0921e82acf277603c4f9c7153
algernon-A <aeneas@aloku.net>
2021-09-19 10:06:46 +1000

    Change serializer class name.

--- ZoningAdjuster/Code/ZoneBlockData/Serializer.cs
+++ ZoningAdjuster/Code/ZoneBlockData/Serializer.cs
@@ -31,7 +31,7 @@ namespace ZoningAdjuster
                 BinaryFormatter formatter = new BinaryFormatter();
 
                 // Serialise savegame settings.
-                DataSerializer.Serialize(stream, DataSerializer.Mode.Memory, CurrentDataVersion, new RealPopSerializer());
+                DataSerializer.Serialize(stream, DataSerializer.Mode.Memory, CurrentDataVersion, new ZoningAdjusterSerializer());
 
                 // Write to savegame.
                 serializableDataManager.SaveData(dataID, stream.ToArray());
@@ -62,7 +62,7 @@ namespace ZoningAdjuster
                     BinaryFormatter formatter = new BinaryFormatter();
 
                     // Deserialise savegame settings.
-                    DataSerializer.Deserialize<RealPopSerializer>(stream, DataSerializer.Mode.Memory);
+                    DataSerializer.Deserialize<ZoningAdjusterSerializer>(stream, DataSerializer.Mode.Memory);
 
                     Logging.Message("read ", stream.Length.ToString());
                 }
